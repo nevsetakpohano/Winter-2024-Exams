807834ac2f6a1bdf46f63fd13ef9dfbc47a5e28a
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 01:57:38 +0200

    Moved variable to beginning

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -3,10 +3,9 @@
 let compareTwoDictionaries = (first_values, second_values) => {
   const arrOfFirstKeys = Object.keys(first_values);
   const arrOfSecondKeys = Object.keys(second_values);
+  let boolFlag = true;
 
   if (arrOfFirstKeys.join('-') !== arrOfSecondKeys.join('-')) return false;
-
-  let boolFlag = true;
 
   for (const element of arrOfFirstKeys) {
     if (first_values[element] === second_values[element]) {
