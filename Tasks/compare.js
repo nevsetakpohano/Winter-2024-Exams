e27fb4c1f92bf83d9dcdc331ad7b57878e00c5d3
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 01:58:29 +0200

    Added 'use strict'

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,12 +1,13 @@
 // Compare two dictionaries
 
+'use strict'
 let compareTwoDictionaries = (first_values, second_values) => {
   const arrOfFirstKeys = Object.keys(first_values);
   const arrOfSecondKeys = Object.keys(second_values);
   let boolFlag = true;
-
+  
   if (arrOfFirstKeys.join('-') !== arrOfSecondKeys.join('-')) return false;
-
+  
   for (const element of arrOfFirstKeys) {
     if (first_values[element] === second_values[element]) {
       boolFlag &&= true;
