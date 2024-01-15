ba66d439f856fd2b74935956cd61de5d3c97722d
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 02:21:04 +0200

    Fixed naming

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,8 +1,7 @@
 // Get one random element from an array
-
-const SAMPLE = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+'use strict'
+const sample = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
-module.exports = SAMPLE;
+module.exports = sample;
