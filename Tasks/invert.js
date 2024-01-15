f0a03add9e6523636b43364bc56c7e766a8088e3
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 02:31:01 +0200

    Update invert.js

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,11 +1,11 @@
 // Reverse an array, you can't use .reverse()
 
-invert = (A) => {
-  T = [];
+'use strict'
+const invert = (A) => {
+  let T = [];
   for(let i=0; i < A.length; i++){
     T.unshift(A[i]);
   }
   return T;
 };
-
 module.exports = invert;
