7aa7b785a12a5cd948074fae2acef87354501d89
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 02:29:28 +0200

    Deleted unnecessary arguments

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,9 +1,10 @@
 // Reverse an array, you can't use .reverse()
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
+
+invert = (A) => {
+  T = Object.keys(A);
+  T.forEach((i) => {
     T[i] = A.pop();
-  }, 7);
+  });
   return T;
 };
 
