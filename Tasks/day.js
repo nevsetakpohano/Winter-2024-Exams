83ac1df699d8882e3daa28c3a09cf95be3963b2d
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 02:17:06 +0200

    Added 'use strict'

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,13 +1,16 @@
 // Get day number
 
-
+'use strict'
 const numDay = (dayString) => {
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+
   for (let i = 0; i < daysOfWeek.length; i++) {
     if (dayString.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
+  
 };
 module.exports = numDay;
