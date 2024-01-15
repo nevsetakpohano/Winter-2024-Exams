7ac1b387706978f7c2bc51f4db5fffe0e4df86a0
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 02:25:52 +0200

    Added 'use strict', used const for for variables

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,13 +1,12 @@
 // Find an intersection of two dictionaries
-
+'use strict'
 function intersectionOfTwoDictionaries(firstObject, secondObject) {
-  keys = Object.keys(firstObject);
-  for (element of keys) {
+  const keys = Object.keys(firstObject);
+  for (const element of keys) {
     if (firstObject[element] != secondObject[element]) {
       delete firstObject[element];
   }
 }
 return firstObject;
 };
-
 module.exports = intersectionOfTwoDictionaries;
