95b32059caa8e0179b28194da080280200d7a744
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-15 02:12:39 +0200

    Changed loop to a function, improved readability

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,13 +1,11 @@
-// Return an array without duplicates
+// Return an array with duplicates
 
 arrWithDuplicates = (value, size) => {
-  if (size <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < size; i++) {
-      res[i] = value;
-    }
-    return res;
+  if (size <= 0) {
+    return [];
+  } else {
+    const newArr = Array.from({ length: size }, () => value);
+    return newArr;
   }
 };
 
