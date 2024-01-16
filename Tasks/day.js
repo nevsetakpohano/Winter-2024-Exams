e50d97ec232b0c2ba33833d490b86ae08012e323
nevsetakpohano <155773389+nevsetakpohano@users.noreply.github.com>
2024-01-16 17:47:31 +0200

    day.js Moved array out of function

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,13 @@
 // Get day number
 
-'use strict'
+"use strict";
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const numDay = (dayString) => {
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-
   for (let i = 0; i < daysOfWeek.length; i++) {
     if (dayString.startsWith(daysOfWeek[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
-  
 };
 module.exports = numDay;
